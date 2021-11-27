52006bfbbb826f899ffa1ab074a3f9e3859bb844
rrerusu <rrerusu@gmail.com>
2021-11-27 17:23:53 -0600

    Modularized methods for later use (reverting changes)

--- SBR.py
+++ SBR.py
@@ -8,19 +8,26 @@ def copy_new_image(new_image_path, old_image_path):
     print("Changed image of {} to {}".format(old_image_path.split("\\")[-2], new_image_path.split("\\")[-1]))
 
 
-# Check to see if image is modified or not
-def is_image_modified(songs_folder, song_folder):
-    return len([i for i in os.listdir(songs_folder) if i.rindex(song_folder) > 0 and i != "1011011 nekodex - new beginnings"]) > 0
+# Determine path of image file
+def determine_image_path(folder_name):
+    song_path = songs_path + "\\{}".format(folder_name)
+    image = [i for i in os.listdir(song_path) if i[-4:] == ".jpg" or i[-4:] == ".png" or i[-5:] == ".jpeg"]
+    return song_path + "\\" + image[0]
+
+
+
+# Single method to modify a single image for a single song
+def modify_image(song_name):
+    old_img_path = determine_image_path(song_name)
+    move_images(old_img_path, move_imgs)
+    copy_new_image(input_img, old_img_path)
 
 
 # Move images into another folder and change name
-def move_images(folder_name):
-    song_path = songs_path + "\\{}".format(folder_name)
-    image = [i for i in os.listdir(song_path) if i[-4:] == ".jpg" or i[-4:] == ".png" or i[-5:] == ".jpeg"]
-    image_path = song_path + "\\" + image[0]
-    os.rename(image_path, move_imgs + "\\{} _ {}".format(folder_name, image[0]))
+def move_images(old_image_path, backup_dir):
+    folder_name = old_image_path.split("\\")[-2]
+    os.rename(old_image_path, backup_dir + "\\{} _ {}".format(folder_name, old_image_path.split("\\")[-1]))
     print("Moved image of song: {}".format(folder_name))
-    return image_path
 
 
 choice = input("Enter 1 to replace, 2 to restore, and anything else to exit: ")
@@ -28,23 +35,23 @@ print("")
 
 # Vars for both methods
 # TODO: Get user input for song and image locations
+# TODO: Check to make sure osu.exe is in folder (Safety)
 songs_path = __file__[:-6] + "Songs"                                                                                # Path of folders that contains songs
 move_imgs = __file__[:-6] + "old_image"                                                                             # Path of folder that contains backup of images
 input_img = "C:\\Users\\rrerusu\\Downloads\\Testing Folder\\change_image\\louis.jpg"                                # Path of image to use to change song images
+songs = [i for i in os.listdir(songs_path) if i != "1011011 nekodex - new beginnings"]                              # Collect list of songs (folders in Songs folder)
 
+# Update ALL images to given image
 if choice == "1":
-    # Vars
-    songs = [i for i in os.listdir(songs_path) if i != "1011011 nekodex - new beginnings"]                          # Collect list of songs (folders in Songs folder)
-
-    # Change each image
     for file in songs:
         try:
-            original_img_path = move_images(file)
-            copy_new_image(input_img, original_img_path)
+            modify_image(file)
             print("")
         except(FileExistsError):
             print("Image for {} is already modified.\n".format(file))
+# Check each folder and return image back to location
 elif choice == "2":
-    pass
+    for file in songs:
+        pass
 
 print("Exiting")
